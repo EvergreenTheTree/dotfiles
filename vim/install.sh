a084b8784a9becea1fe0254bf5ea3d8ba9d586f6
4Evergreen4 <4Evergreen4@users.noreply.github.com>
2016-10-01 17:20:45 -0400

    Modify vim config install script to be posix complient

--- vim/install.sh
+++ vim/install.sh
@@ -1,64 +1,86 @@
-#!/usr/bin/env bash
-usage="Usage: install.sh [-h] [-vn]"
-read -r -d '' help <<EOF
+#!/bin/sh
+usage="Usage: install.sh [-h] [-nv]"
+help=$(cat <<EOF
+Install Evergreen's vim configuration.
+
 $usage
 
-  -h, --help  show this help message
-  -v          install vim config files
-  -n          install nvim config files
+  -h  show this help message
+  -n  install nvim config files
+  -v  install vim config files
 EOF
+)
+optstring=":hnv"
+SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
 
-if (($# < 1)); then
-	echo >&2 "Invalid number of arguments.  At least 1 argument required."
-	echo >&2 "$usage"
+### FUNCTIONS ###
+# Show an error message along with a short usage message and exit.
+error() {
+	printf 'ERROR: %s\n' "$1" >&2
+	printf '%s\n' "$usage" >&2
 	exit 1
-elif [[ $# -eq 1 ]] && [[ "$1" = "-h" || "$1" = "--help" ]]; then
-	echo "$help"
-	exit 0
+}
+
+fexists() {
+	if [ -e "$1" ]; then
+		error "$1 already exists, not overwriting"
+	fi
+}
+
+# Install vim config files to given path.
+install_config() {
+	check_exists "$1"
+	printf '\n'
+	ln -s "$SCRIPT_DIR" "$1"
+	printf 'Installed config files at %s' "$1"
+}
+
+### OPTION HANDLING ###
+if [ $# -lt 1 ]; then
+	error "at least 1 argument required"
 fi
 
-# Determine what the install type is.
-install_type="" # v=vim, n=nvim, nv/vn=vim and nvim
-while getopts ":vn" opt; do
-	case "$opt" in
+install_neovim=false
+install_vim=false
+while getopts $optstring opt; do
+	case $opt in
 		v)
-			install_type+="v"
+			install_vim=true
 			;;
 		n)
-			install_type+="n"
+			install_neovim=true
+			;;
+		h)
+			printf '%s\n' "$help"
+			exit 0
+			;;
+		\?)
+			error "invalid option: \"-$OPTARG\""
+			;;
+		:)
+			error "option \"-$OPTARG\" requires an argument"
 			;;
 	esac
 done
 
-check_exists() {
-	# Check if file exists.
-	if [[ -e $1 ]]; then
-		echo >&2 "Error: $1 already exists.  Please remove it and try again."
-		exit 1
-	fi
-}
-
-install_config() {
-	# Install vim config files to given path.
-	check_exists "$1"
-	echo
-	script_dir="$(dirname "$(readlink -f "$0")")"
-	ln -s "$script_dir" "$1"
-	echo "Installed config files at $1."
-}
-
-if [[ $install_type = *"v"* ]]; then # install_type contains 'v'
-	echo "Installing vim config files."
-	echo
-	check_exists ~/.vimrc
+### SCRIPT ###
+if [ $install_vim = true ]; then
+	printf 'Installing vim config files.\n'
+	printf '\n'
+	fexists ~/.vim
+	fexists ~/.vimrc
 	install_config ~/.vim
 fi
 
-if [[ $install_type = *"n"* ]]; then # install_type contains 'n'
-	echo "Installing nvim config files."
-	echo
+if [ $install_neovim = true ]; then
+	printf 'Installing neovim config files.\n'
+	printf '\n'
+	fexists ~/.config/nvim
 	install_config ~/.config/nvim
 fi
 
-echo
-echo "Done."
+printf '\n'
+printf 'Done.\n'
+
+## vim:fdm=expr:fdl=0:fdc=3:
+## vim:fde=getline(v\:lnum)=~'^##'?'>'.(matchend(getline(v\:lnum),'##*')-2)\:'='
