11ad1d42e10304cfa50fa5616c1906fdba75cffc
4Evergreen4 <4Evergreen4@users.noreply.github.com>
2019-11-12 17:04:25 -0500

    Add qutebrowser config

---
+++ qutebrowser/config.py
@@ -0,0 +1,25 @@
+c.fonts.monospace = '"Misc Ohsnap", "xos4 Terminus"'
+c.fonts.completion.category = "bold 12pt monospace"
+c.fonts.completion.entry = "12pt monospace"
+c.fonts.debug_console = "12pt monospace"
+c.fonts.downloads = "12pt monospace"
+c.fonts.hints = "bold 12pt monospace"
+c.fonts.keyhint = "12pt monospace"
+c.fonts.messages.error = "12pt monospace"
+c.fonts.messages.info = "12pt monospace"
+c.fonts.messages.warning = "12pt monospace"
+c.fonts.prompts = "12pt monospace"
+c.fonts.statusbar = "12pt monospace"
+c.fonts.tabs = "12pt monospace"
+c.fonts.web.family.serif = "Noto Serif"
+c.fonts.web.family.sans_serif = "Noto Sans"
+
+c.session.default_name = "Default"
+c.auto_save.session = True
+c.session.lazy_restore = True
+
+c.content.autoplay = False
+
+c.scrolling.smooth = True
+
+config.source('nord-qutebrowser.py')
