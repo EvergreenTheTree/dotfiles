3f7f755307d2e2680caf5ac02dd7db9571f4d582
4Evergreen4 <4Evergreen4@users.noreply.github.com>
2020-05-08 17:45:35 -0400

    Update qutebrowser font config

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,16 +1,5 @@
-c.fonts.monospace = '"Misc Ohsnap", "xos4 Terminus"'
-c.fonts.completion.category = "bold 12pt monospace"
-c.fonts.completion.entry = "12pt monospace"
-c.fonts.debug_console = "12pt monospace"
-c.fonts.downloads = "12pt monospace"
-c.fonts.hints = "bold 12pt monospace"
-c.fonts.keyhint = "12pt monospace"
-c.fonts.messages.error = "12pt monospace"
-c.fonts.messages.info = "12pt monospace"
-c.fonts.messages.warning = "12pt monospace"
-c.fonts.prompts = "12pt monospace"
-c.fonts.statusbar = "12pt monospace"
-c.fonts.tabs = "12pt monospace"
+c.fonts.default_family = 'Misc Ohsnap'
+c.fonts.default_size = '11pt'
 c.fonts.web.family.serif = "Noto Serif"
 c.fonts.web.family.sans_serif = "Noto Sans"
 
@@ -21,5 +10,7 @@ c.session.lazy_restore = True
 c.content.autoplay = False
 
 c.scrolling.smooth = True
+
+c.editor.command = ["nvim-qt", "{file}", "--", "-c", "normal {line}G{column0}l"]
 
 config.source('nord-qutebrowser.py')
