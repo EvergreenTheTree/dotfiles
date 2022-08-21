5db05c6b2cd4223281322cdef5ae14d83716c415
4Evergreen4 <4Evergreen4@users.noreply.github.com>
2022-08-21 15:24:41 -0400

    Polybar directory cleanup

--- polybar/launcher.sh
+++ polybar/launcher.sh
@@ -7,5 +7,5 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload primary &
 done
