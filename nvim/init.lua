acb8dcf749b840ef8d38f729b0b23007bd9487c6
EvergreenTheTree <EvergreenTheTree@users.noreply.github.com>
2024-08-25 11:48:51 -0500

    nviom switch to manual completion triggering

--- nvim/init.lua
+++ nvim/init.lua
@@ -115,6 +115,19 @@ lspconfig.lua_ls.setup {
 }
 lspconfig.clangd.setup{}
 
+require'lspconfig'.pylsp.setup{
+    settings = {
+        pylsp = {
+            plugins = {
+                pycodestyle = {
+                    ignore = {'W391'},
+                    maxLineLength = 100
+                }
+            }
+        }
+    }
+}
+
 require "init.diagnostic_delay"
 vim.diagnostic.config({
     virtual_text = true,
@@ -165,6 +178,12 @@ vim.api.nvim_create_autocmd("LspAttach", {
 })
 
 -- nvim-cmp setup
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+
 local cmp = require "cmp"
 cmp.setup {
     completion = {
@@ -180,20 +199,25 @@ cmp.setup {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         },
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            else
-                fallback()
+        ['<Tab>'] = function(fallback)
+            if not cmp.select_next_item() then
+                if vim.bo.buftype ~= 'prompt' and has_words_before() then
+                    cmp.complete()
+                else
+                    fallback()
+                end
             end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            else
-                fallback()
+        end,
+
+        ['<S-Tab>'] = function(fallback)
+            if not cmp.select_prev_item() then
+                if vim.bo.buftype ~= 'prompt' and has_words_before() then
+                    cmp.complete()
+                else
+                    fallback()
+                end
             end
-        end, { "i", "s" }),
+        end,
     }),
     sources = {
         { name = 'nvim_lsp' },
@@ -356,6 +380,9 @@ require'nvim-treesitter.configs'.setup {
                 ["[]"] = "@class.outer",
             },
         },
+    },
+    indent = {
+        enable = true,
     },
 }
 
